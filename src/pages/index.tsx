61d14bf75490050759b2296c3bc439ad257f3466
Musilah <nataleigh.nk@gmail.com>
2024-12-10 19:47:44 +0300

    adjust primary blue shade and metadata
    Signed-off-by: Musilah <nataleigh.nk@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,8 +35,8 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title="Homepage"
+      description="SuperMQ is the scalable, secure IoT Cloud Platform. Get started with SuperMQ today!"
     >
       <HomepageHeader />
       <main>
